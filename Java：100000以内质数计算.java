47a7f3a797cfaab60f75be2ed8164364e893d204
八层夹心奥利奥 <83682478+TomorrowMC@users.noreply.github.com>
2021-07-25 14:15:08 +0800

    Create Java：100000以内质数计算.java

--- Java：100000以内质数计算.java
+++ Java：100000以内质数计算.java
@@ -1,11 +1,11 @@
 //获取当前时间距离1970-01-0100:00:00的毫秒数
 long start = System.currentTimeMillis();
 label:for(int i =2;i<=100000;i++){//遍历100000以内的自然数
-	for(int j =2;j <= Math.sqrt(i);j++){  //j:被i去除
+	for(int j =2;j <= Math.sqrt(i);j++){  //j:被i去除，这里使用开方的话可以有效减少计算
 		if(i%j== 0){  //i被j除尽
 			continue label;
-		}/能执行到此步骤的，都是质数
-		count++;
+		}//能执行到此步骤的，都是质数
+		count++;    
 //获取当前时间距离1970-01-8100:00:00的毫秒数
 long end = System.currentTimeMillis();
 System.out println("质数的个数为:”+ count);
